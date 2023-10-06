85280c0ff14dae849dc62202fcd5a02e204dcba3
Jon Hunt <hun19007@byui.edu>
2023-10-06 09:32:34 -0600

    Update 10/6/2023

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Create a Scripture object
+        Scripture myScripture = new Scripture("Helamen 5:12", "And now, my sons, remember, remember that it is upon the arock of our Redeemer, who is Christ, the Son of God, that ye must build your bfoundation; that when the devil shall send forth his mighty winds, yea, his shafts in the whirlwind, yea, when all his hail and his mighty cstorm shall beat upon you, it shall have no power over you to drag you down to the gulf of misery and endless wo, because of the rock upon which ye are built, which is a sure foundation, a foundation whereon if men build they cannot fall.");
+
+        // Display the complete scripture
+        myScripture.Display();
+
+        // Continue hiding words until all are hidden
+        while (!myScripture.AllWordsHidden)
+        {
+            // Prompt the user to press Enter or type quit
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to exit.");
+            string userInput = Console.ReadLine();
+
+            // Check if the user wants to quit
+            if (userInput.ToLower() == "quit")
+                break;
+
+            // Hide a few random words and display the scripture
+            myScripture.HideRandomWords();
+            myScripture.Display();
+        }
+
+        Console.WriteLine("Program ended.");
     }
 }
+
+
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,19 +1,53 @@
-public class Scripture
+class Scripture
 {
-    public string _book;
-    public string _chapter;
-    public string _verse;
-    public string _txt;
+    private string reference;
+    private string text;
+    private List<string> hiddenWords;
+    private string[] allWords;
 
-
-
-    // Constructor 
-    public Scripture (string book, string chapter, string verse, string txt) {
-        _book = book;
-        _chapter = chapter;
-        _verse = verse;
-        _txt = txt;
-
+    public Scripture(string reference, string text)
+    {
+        this.reference = reference;
+        this.text = text;
+        this.hiddenWords = new List<string>();
+        this.allWords = text.Split(' ');
     }
 
+    public void Display()
+    {
+        Console.Clear();
+        Console.WriteLine($"{reference}\n");
+
+        foreach (var word in allWords)
+        {
+            if (hiddenWords.Contains(word))
+                Console.Write("____ ");
+            else
+                Console.Write($"{word} ");
+        }
+
+        Console.WriteLine("\n");
+    }
+
+    public void HideRandomWords()
+    {
+        // Randomly hide a few words
+        Random random = new Random();
+        int wordsToHide = random.Next(1, 4);
+
+        for (int i = 0; i < wordsToHide; i++)
+        {
+            int randomIndex = random.Next(allWords.Length);
+            string wordToHide = allWords[randomIndex];
+
+            // Check if the word is not already hidden
+            if (!hiddenWords.Contains(wordToHide))
+                hiddenWords.Add(wordToHide);
+        }
+    }
+
+    public bool AllWordsHidden
+    {
+        get { return hiddenWords.Count == allWords.Length; }
+    }
 }
